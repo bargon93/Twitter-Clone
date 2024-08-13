0b03c4292ea346aa4c54d80bc4a7f44181cd0094
Bar.Gonen <bar.g555@gmail.com>
2024-08-13 11:27:49 +0300

    generate token and set cookie

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from 'bcryptjs';
 import User from "../models/user.model.js";
+import tokenSetUp from "../utils/tokenSetUp.js"
 
 export const signup = async (req, res) => {
     try {
--- backend/utils/tokenSetUp.js
+++ backend/utils/tokenSetUp.js
@@ -4,4 +4,11 @@ export const tokenSetUp = (userID, res) => {
     const token = jwt.sign({userID}, process.env.JWT_SECRET,{
         expiresIn: '1d'
     })
+
+    res.cookie("jwt", token, {
+        maxAge: 24 * 60 * 60 * 1000, //MS
+        httpOnly: true, //prevent scripting attacks
+        sameSite: "strict", //prevent cross-site request forgery attacks
+        secure: process.env.NODE_ENV !== "development",
+    });
 }
