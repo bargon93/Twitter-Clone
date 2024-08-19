c6c9de96ca3b2a464485e12aef25c8c555c237b8
Bar.Gonen <bar.g555@gmail.com>
2024-08-19 15:04:02 +0300

    Implementing deleteing posts

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -27,7 +27,31 @@ export const createPost = async (req, res) => {
         await newPost.save();
 
         res.status(201).json(newPost);
+
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({error: "Internal server error"});
+    }
+}
+
+export const deletePost = async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+        if(!post) return res.status(404).json({error: "Post not found"});
+
+        if(post.user.toString() !== req.user._id.toString()) {
+            return res.status(401).json({error: "Can not delete other users' posts"});
+        }
+
+        if(post.img) {
+            const imgID = post.img.split("/").pop().split(".")[0];
+            await cloudinary.uploader.destroy(imgID);
+        }
         
+        await Post.findByIdAndDelete(req.params.id);
+
+        res.status(200).json({message : "Post deleted successfully"});
+
     } catch (error) {
         console.log(error.message);
         res.status(500).json({error: "Internal server error"});
