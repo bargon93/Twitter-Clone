642aabb683330944151ec0e626e3c89e18e59e51
Bar.Gonen <bar.g555@gmail.com>
2024-08-20 10:07:11 +0300

    Post functionality

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -57,3 +57,35 @@ export const deletePost = async (req, res) => {
         res.status(500).json({error: "Internal server error"});
     }
 }
+
+export const commentOnPost = async (req, res) => {
+    try {
+        const {text} = req.body;
+        const postID = req.params.id;
+        const userID = req.user._id;
+
+        if(!text) return res.status(400).json({error: "Nothing to comment"});
+
+        const post = await Post.findById(postID);
+
+        if(!post) return res.status(404).json({error: "Post not found"});
+
+        const newComment = {user: userID, text};
+        post.comments.push(newComment);
+        await post.save();
+
+        res.status(200).json(post);
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({error: "Internal server error"});
+    }
+}
+
+export const likeUnlikePost = async (req, res) => {
+    try {
+        
+    } catch (error) {
+        console.log(error.message);
+        res.status(500).json({error: "Internal server error"});
+    }
+}
--- backend/routes/post.routes.js
+++ backend/routes/post.routes.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import { protectRoute } from '../middlewares/protectRoute.middleware.js';
-import { createPost, deletePost } from '../controllers/post.controller.js';
+import { createPost, deletePost, commentOnPost, likeUnlikePost } from '../controllers/post.controller.js';
 
 const router = express.Router();
 
 router.post("/create", protectRoute, createPost);
-// router.post("/like/:id", protectRoute, likeUnlikePost);
-// router.post("/comment/:id", protectRoute, commentOnPost);
+router.post("/like/:id", protectRoute, likeUnlikePost);
+router.post("/comment/:id", protectRoute, commentOnPost);
 router.delete("/:id", protectRoute, deletePost);
 
 
