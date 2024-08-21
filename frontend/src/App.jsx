e1899d4593c61049943cdf8900fdfa225382d06a
Bar.Gonen <bar.g555@gmail.com>
2024-08-21 09:53:57 +0300

    Implementing sign up page UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
 import { Route, Routes } from "react-router-dom"
+import HomePage from "./pages/home/HomePage";
+import LoginPage from "./pages/auth/login/LoginPage";
+import SignUpPage from "./pages/auth/signup/SignUpPage";
 
 function App() {
   return (
     <div className='flex max-w-6xl mx-auto'>
-			<Routes>
-				<Route path='/' element={<HomePage />} />
-				<Route path='/signup' element={<SignUpPage />} />
-				<Route path='/login' element={<LoginPage />} />
-			</Routes>
-		</div>
+		<Routes>
+			<Route path='/' element={<HomePage />} />
+			<Route path='/signup' element={<SignUpPage />} />
+			<Route path='/login' element={<LoginPage />} />
+		</Routes>
+	</div>
   )
 }
 
-export default App
+export default App;
