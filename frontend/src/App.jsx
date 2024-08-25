5cfce523c3f1eae0add7c126f4679eab42a57bc5
Bar.Gonen <bar.g555@gmail.com>
2024-08-25 12:14:14 +0300

    Implementing right panel UI and skeleton

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,15 +2,19 @@ import { Route, Routes } from "react-router-dom"
 import HomePage from "./pages/home/HomePage";
 import LoginPage from "./pages/auth/login/LoginPage";
 import SignUpPage from "./pages/auth/signup/SignUpPage";
+import SideBar from "./components/common/SideBar";
+import RightPanel from "./components/common/RightPanel";
 
 function App() {
   return (
     <div className='flex max-w-6xl mx-auto'>
+		<SideBar/>
 		<Routes>
 			<Route path='/' element={<HomePage />} />
 			<Route path='/signup' element={<SignUpPage />} />
 			<Route path='/login' element={<LoginPage />} />
 		</Routes>
+		<RightPanel/>
 	</div>
   )
 }
