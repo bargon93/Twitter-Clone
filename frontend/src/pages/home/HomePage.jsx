e895522f86616714531b199df439bcccebc0bed9
Bar.Gonen <bar.g555@gmail.com>
2024-08-25 12:58:55 +0300

    Implementing Post UI

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-import Posts from "../../components/common/Posts";
+import Posts from "../../components/common/Post";
 import CreatePost from "./CreatePost";
 
 const HomePage = () => {
