848d6d33dbc9632ec2735ce771036b44f3d99825
Bar.Gonen <bar.g555@gmail.com>
2024-08-25 12:37:54 +0300

    //

--- frontend/src/pages/home/HomePage.jsx
+++ frontend/src/pages/home/HomePage.jsx
@@ -15,7 +15,7 @@ const HomePage = () => {
             className="flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 cursor-pointer relative"
             onClick={() => {setFeedType("forYou")}}
           >
-            For forYou
+            For You
             {feedType === "forYou" && (
               <div className="absolute bottom-0 w-10 h-1 rounded-full bg-primary"></div>
             )}
