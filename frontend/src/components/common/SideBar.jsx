159b7770eda8ecdc0168c8a5b81703f2ce35be0e
Bar.Gonen <bar.g555@gmail.com>
2024-08-29 11:08:41 +0300

    Log out functionality implementation

--- frontend/src/components/common/SideBar.jsx
+++ frontend/src/components/common/SideBar.jsx
@@ -5,16 +5,37 @@ import { IoNotifications } from "react-icons/io5";
 import { FaUser } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { BiLogOut } from "react-icons/bi";
+import { useMutation } from "@tanstack/react-query"
+import { toast } from "react-hot-toast"
 
 const SideBar = () => {
-    const data = {
-        fullName: "mock fullname",
-        username: "mockusername",
-        profileImg: "/avatars/boy1.png",
-    };
 
-    return (
-        <div className='md:flex-[2_2_0] w-18 max-w-52'>
+	const {mutate: logoutMutation, isPending, isError, error} = useMutation({
+		mutationFn: async () => {
+			try {
+				const res = await fetch("api/auth/logout", {
+					method: "POST"
+				})
+				const data = await res.json();
+
+				if(!res.ok) throw new Error(data.error || "Something went wrong");
+			} catch (error) {
+				throw new Error(error);
+			}
+		},
+		onSuccess: () => {
+			toast.success("Logged out successfully");
+		}
+	})
+
+	const data = {
+		fullName: "mock fullname",
+		username: "mockusername",
+		profileImg: "/avatars/boy1.png",
+	};
+
+	return (
+		<div className='md:flex-[2_2_0] w-18 max-w-52'>
 			<div className='sticky top-0 left-0 h-screen flex flex-col border-r border-gray-700 w-20 md:w-full'>
 				<Link to='/' className='flex justify-center md:justify-start'>
 					<XSvg className='px-2 w-12 h-12 rounded-full fill-white hover:bg-stone-900' />
@@ -64,13 +85,18 @@ const SideBar = () => {
 								<p className='text-white font-bold text-sm w-20 truncate'>{data?.fullName}</p>
 								<p className='text-slate-500 text-sm'>@{data?.username}</p>
 							</div>
-							<BiLogOut className='w-5 h-5 cursor-pointer' />
+							<BiLogOut className='w-5 h-5 cursor-pointer' 
+								onClick={(e) => {
+									e.preventDefault();
+									logoutMutation();
+								}}
+							/>
 						</div>
 					</Link>
 				)}
 			</div>
 		</div>
-    );
+	);
 };
 
 export default SideBar;
